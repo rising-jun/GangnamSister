cfecf1717192b5729cd2967082231aef7829a78f
kimdongjune <jun36817@naver.com>
2020-11-09 18:44:26 +0900

    Initial Commit

---
+++ BasicMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicMVVM
+//
+//  Created by 김동준 on 2020/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
